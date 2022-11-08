1ad56390d5a59d3cdfcc5f6099940ff1e8c5e862
JSuggu <azamefranco@gmail.com>
2022-11-08 14:23:47 -0300

    index done, routes file created

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require ("express");
+const app = express;
+const routes = require ("./routes/routes");
+
+//MIDDLEWARES
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+//CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, enc-entype, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
+//RUTAS Y CONEXION
+
+app.listen(3000, (req, res) => {
+    console.log("servidor iniciado");
+})
+
+app.use("/", routes);
